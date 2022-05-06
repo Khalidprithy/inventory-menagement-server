4033778b19af1821e51543f06eb42a87e60a75b5
Khalidprithy <khalidprithy@gmail.com>
2022-05-06 23:25:47 +0600

    server connected

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const express = require('express');
 const cors = require('cors');
 const port = process.env.PORT || 5000;
@@ -10,10 +11,40 @@ require('dotenv').config();
 app.use(cors());
 app.use(express.json());
 
+
+//  user1
+// KOVOGlSVRlylG7OS
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.tu7gy.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        await client.connect();
+        const productCollection = client.db('inventoryApp').collection('product');
+
+        app.get('/product', async (req, res) => {
+            const query = {};
+            const cursor = productCollection.find(query);
+            const products = await cursor.toArray();
+            res.send(products);
+        });
+
+    } finally {
+        // await client.close();
+    }
+}
+
+run().catch(console.dir)
+
+
+
+
 app.get('/', (req, res) => {
     res.send('Inventory app server running')
 });
 
 app.listen(port, () => {
-    console.log('Listening to server', port)
+    console.log('Listening to inventory server', port)
 })
