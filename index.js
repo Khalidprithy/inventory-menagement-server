d8a734f066bd4117292919d1b5a38b72c300500d
Khalidprithy <khalidprithy@gmail.com>
2022-05-13 18:45:37 +0600

    delete one added

--- index.js
+++ index.js
@@ -13,10 +13,6 @@ app.use(cors());
 app.use(express.json());
 
 
-//  user1
-// KOVOGlSVRlylG7OS
-
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.tu7gy.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
@@ -40,6 +36,22 @@ async function run() {
             const product = await productCollection.findOne(query);
             res.send(product);
         })
+
+        // Add Product
+        app.post('/products', async (req, res) => {
+            const newProduct = req.body;
+            const result = await productCollection.insertOne(newProduct);
+            res.send(result)
+        })
+
+        // Delete Product
+        app.delete('/products/:id', async (req, res) => {
+            const id = req.params;
+            const query = { _id: ObjectId(id) };
+            const result = await productCollection.deleteOne(query);
+            res.send(result)
+        });
+
 
 
     } finally {
